e5475b64e10c6c3b247a71520bd5875dfaf68d64
Juan Saldana <saldanaj97@gmail.com>
2022-11-08 16:03:30 -0600

    Scenes now displayed correctly with test data

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Lights from "./Components/Lights";
+import Scenes from "./Components/Scenes";
 
 function App() {
   return (
     <div className='App'>
       <Lights />
+      <Scenes />
     </div>
   );
 }
---
+++ Client/src/Components/Scenes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/button";
+import { Box, Heading } from "@chakra-ui/layout";
+
+export default function Scenes() {
+  let myScenes = [
+    { id: 1, name: "Scene 1" },
+    { id: 2, name: "Scene 2" },
+    { id: 3, name: "Scene 3" },
+    { id: 4, name: "Scene 4" },
+    { id: 5, name: "Scene 5" },
+  ];
+
+  return (
+    <div className='scenes-container'>
+      <Heading className='scenes-heading'>My Scenes</Heading>
+      <Box className='scenes-list'>
+        {myScenes.map((scene) => {
+          return (
+            <Button key={scene.id} color='black' background='red'>
+              <Text>{scene.name}</Text>{" "}
+            </Button>
+          );
+        })}
+      </Box>
+    </div>
+  );
+}
