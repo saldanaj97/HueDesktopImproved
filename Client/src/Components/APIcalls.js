45ec65caac55bfc023726d3bc9848bbf9622ddef
Juan Saldana <saldanaj97@gmail.com>
2022-11-08 16:55:02 -0600

    User scenes now correctly fetched from backend

--- Client/src/Components/APIcalls.js
+++ Client/src/Components/APIcalls.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_LIGHTS_API_URL || "http://localhost:3001";
+const BRIDGE_API_URL = process.env.REACT_APP_LIGHTS_API_URL || "http://localhost:3001/bridge";
 
 // API call to get the users lights
 export const getLights = async () => {
-  const { success, userLights } = await axios.get(API_URL + "/bridge/lights").then((response) => response.data);
+  const { success, userLights } = await axios.get(BRIDGE_API_URL + "/lights").then((response) => response.data);
   return { success, userLights };
+};
+
+// API call to get the users scenes
+export const getScenes = async () => {
+  const { success, userScenes } = await axios.get(BRIDGE_API_URL + "/scenes").then((response) => response.data);
+  return { success, userScenes };
 };
--- Client/src/Components/Scenes.js
+++ Client/src/Components/Scenes.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/button";
 import { Box, Heading } from "@chakra-ui/layout";
+import { getScenes } from "./APIcalls";
 
 export default function Scenes() {
+  const [scenes, setScenes] = useState([]);
+
+  useEffect(() => {
+    getUserScenes();
+  });
+
+  const getUserScenes = async () => {
+    const data = await getScenes();
+  };
+
   let myScenes = [
     { id: 1, name: "Scene 1" },
     { id: 2, name: "Scene 2" },
