a7895aa1c89be63faee51ab0cf3302ad184efbda
Juan Saldana <saldanaj97@gmail.com>
2022-11-09 12:48:00 -0600

    Added the set scene call

--- Client/src/Components/APIcalls.js
+++ Client/src/Components/APIcalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BRIDGE_API_URL = process.env.REACT_APP_LIGHTS_API_URL || "http://localhost:3001/bridge";
+const BRIDGE_API_URL = process.env.REACT_APP_BRIDGE_URL || "http://localhost:3001/bridge";
 
 // API call to get the users lights
 export const getLights = async () => {
@@ -12,4 +12,10 @@ export const getLights = async () => {
 export const getScenes = async () => {
   const { success, userScenes } = await axios.get(BRIDGE_API_URL + "/scenes").then((response) => response.data);
   return { success, userScenes };
+};
+
+// Function to carry out the changing of a scene
+export const setNewScene = async (id) => {
+  const { success } = await axios.post(BRIDGE_API_URL + "/scene_change", { id: id }).then((response) => response.data);
+  return success;
 };
