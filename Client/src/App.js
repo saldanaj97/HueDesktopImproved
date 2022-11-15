f6a972086e9ab7c16bd1bea5173b04f82e68b042
Juan Saldana <saldanaj97@gmail.com>
2022-11-15 11:36:22 -0600

    Implemented basic settings button with no menu yet

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import Lights from "./Components/Lights";
 import Scenes from "./Components/Scenes";
+import Header from "./Components/Header";
 import "./App.css";
 
 function App() {
   return (
     <ChakraProvider>
       <Box className='App'>
+        <Header />
         <Lights />
         <Scenes />
       </Box>
---
+++ Client/src/Components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Button, Flex, IconButton, Text } from "@chakra-ui/react";
+import { IoMdSettings } from "react-icons/io";
+import { theme } from "../index";
+
+export default function Header() {
+  return (
+    <Box className='header-container'>
+      <Box className='settings-button-container'>
+        <Flex justify='flex-end'>
+          <IconButton
+            as={IoMdSettings}
+            _hover={{
+              color: theme.mainColors[600],
+            }}
+            color={"white"}
+            background={"transparent"}
+            size='md'
+            padding='3px'
+            rounded={"15px"}
+            margin={"5px 5px 0px 0px"}
+          />
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
